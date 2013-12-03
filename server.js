751c59bdfc4915a246cf187b6ddf926f38dfd5b0
mjdean1994 <mjdean1994@gmail.com>
2013-12-02 21:04:43 -0500

    Instagram update.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var ejs = require('ejs');
 var app = express(); 
 var analyze = require('sentimental').analyze;
-
+var Instagram = require('instagram-node-lib');
 var Twitter = require('mtwitter');
 
 var twitter = new Twitter({
@@ -12,6 +12,9 @@ var twitter = new Twitter({
   access_token_secret: 'u7dZuEyXyoWOWQ7LsWzVAzeMpDkVyRhBx426yHTz3LlXb'
 });
 
+
+Instagram.set('client_id', '371053453f2b4f349e542db37e42a420');
+Instagram.set('client_secret', '5f7678cb2c00403eb72d37d5dbcc81fa');
 
 
  
@@ -49,18 +52,19 @@ app.get('/', function(req, res) {
 app.post('/formPost', function(req, res){
 	//console.log(req.body);
 	
-	var model = {
-				name: [],
-				picture: [],
-				text: [],
-				sentiment: []
-			}
+	
 	
 	if(req.body.source == "twitter")
 		{
 		twitter.get('search/tweets', {q: req.body.hashtag1}, function(err, item) {
 			
 			var i = 0
+			var model = {
+				name: [],
+				picture: [],
+				text: [],
+				sentiment: []
+			}
 			for(; i<item.statuses.length && i<10;i++)
 			{
 				model.name[i] = item.statuses[i].user.name;
@@ -83,7 +87,84 @@ app.post('/formPost', function(req, res){
 	}
 	else if(req.body.source == "instagram")
 	{
-		//INSTAGRAM SEARCH CODE HERE//
+		Instagram.tags.recent({ name: req.body.hashtag1, complete: function(data, pagination ) 
+        {
+			var i = 0
+			var model = {
+				name: [],
+				profPic: [],
+				picture: [],
+				text: [],
+				sentiment: []
+			}
+			for(; i<data.length && i<10;i++)
+			{
+				model.name[i] = data[i].user.username;
+				model.text[i] = data[i].caption.text;
+				model.profPic[i] = data[i].user.profile_picture;
+				model.picture[i] = data[i].images.standard_resolution.url;
+				model.sentiment[i] = analyze(model.text[i]).score;
+				
+			}
+			/*var i = 0
+			var model = {
+				name: [],
+				profPic: [],
+				picture: [],
+				text: [],
+				sentiment: []
+				}
+				for(; i<data.length && i<10;i++)
+				{
+					model.name[i] = data[i].user.username;
+					model.profPic[i] = data[i].user.profile_picture;
+					model.text[i] = data[i].caption;
+					model.picture[i] = data[i].images.standard_resolution.url;
+					model.sentiment[i] = analyze(model.text[i]).score;
+				}
+				
+				for(; i<10;i++)
+				{
+					model.name[i] = '';
+					model.picture[i] = '';
+					model.profPic[i] = '';
+					model.text[i] = '';
+					model.sentiment[i] = 0;
+				}
+			
+			res.render('index2.ejs', model);*/
+			
+			res.render('index2.ejs', model);
+                                
+        }
+    });   
+		/*var i = 0
+		var model = {
+				name: [],
+				profPic: [],
+				picture: [],
+				text: [],
+				sentiment: []
+			}
+			for(; i<item.statuses.length && i<10;i++)
+			{
+				model.name[i] = item.data[i].user.username;
+				model.profPic[i] = item.data[i].user.profile_picture;
+				model.text[i] = item.data[i].caption;
+				model.picture[i] = item.data[i].images.standard_resolution.url;
+				model.sentiment[i] = analyze(model.text[i]).score;
+			}
+			
+			for(; i<10;i++)
+			{
+				model.name[i] = '';
+				model.picture[i] = '';
+				model.profPic[i] = '';
+				model.text[i] = '';
+				model.sentiment[i] = 0;
+			}
+			
+			res.render('index2.ejs', model);*/
 	}
 	
 	
